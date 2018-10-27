4b873adf98645cedad6b438ee6742b4105f7fe42
Beast_07 <sloungeking@brandeis.edu>
2018-10-27 18:14:46 -0400

    Skeleton of Basic tunnel is coming along.

--- src/cs131/pa2/CarsTunnels/BasicTunnel.java
+++ src/cs131/pa2/CarsTunnels/BasicTunnel.java
@@ -16,8 +16,15 @@ public class BasicTunnel extends Tunnel{
 	private int activeSled;
 	private int waitingCars;
 	private int waitingSleds;
-
 	
+	
+	private boolean carsShouldPass() {//Returns true if cars should pass this tunnel
+		return (activeCars > 2 || waitingSleds < 1 || activeSled < 1);
+	}
+	
+	private boolean sledShouldPass() {//Returns true if sleds should pass this tunnel 
+		return (activeCars > 0 || waitingSleds > 0 || activeSled > 0);
+	}
 	
 
 	public BasicTunnel(String name) {
@@ -26,11 +33,23 @@ public class BasicTunnel extends Tunnel{
 
 	@Override
 	public boolean tryToEnterInner(Vehicle vehicle) {
+		lock.lock();
+		/*
+		 * Check how many vehicles are in the tunnel
+		 * if no room return false and unlock.
+		 * 
+		 * If there is room, put it in and unlock.
+		 * 
+		 */
 		return false;
 	}
 
 	@Override
 	public void exitTunnelInner(Vehicle vehicle) {
+		/*
+		 * Remove a vehicle of vehicle type from the tunnel.
+		 * Synchronize while removing?
+		 */
 		
 	}
 	
